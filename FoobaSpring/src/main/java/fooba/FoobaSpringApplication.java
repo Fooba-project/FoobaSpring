2a5ac99ccfe83b84e28d89747951179d406bc77d
Isaac Jeong <isacgood@naver.com>
2023-02-08 16:55:44 +0900

    aa

---
+++ FoobaSpring/src/main/java/fooba/FoobaSpringApplication.java
@@ -0,0 +1,13 @@
+package fooba;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FoobaSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FoobaSpringApplication.class, args);
+	}
+
+}
