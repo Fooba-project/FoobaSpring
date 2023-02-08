d9cd49dd81108d8705ed69651679280f387650b3
Isaac Jeong <isacgood@naver.com>
2023-02-08 18:12:19 +0900

    a

--- FoobaSpring/src/main/java/fooba/controller/MemberController.java
+++ FoobaSpring/src/main/java/fooba/controller/MemberController.java
@@ -1,8 +1,21 @@
 package fooba.controller;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
+import javax.validation.Valid;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
+import fooba.dto.MemberVO;
 import fooba.service.MemberService;
 
 @Controller
@@ -10,6 +23,25 @@ public class MemberController {
 
 	@Autowired
 	MemberService ms;
+	
+	/*
+	// 기본 프레임
+	@RequestMapping("/")
+	public String method(@ModelAttribute("dto") @Valid MemberVO mvo, BindingResult result,
+			HttpServletRequest request, HttpSession session, Model model,
+			@RequestParam("인수1") String 인수1) {
+		if(session.getAttribute("loginUser")==null) return "redirect:/login"; // 로그인체크
+
+		HashMap<String, Object> prm = new HashMap<>();
+		prm.put("인수1", 인수1);
+		prm.put("rc", null);
+		ms.servicemethod(hm);
+		ArrayList<HashMap<String,Object>> list = (ArrayList<HashMap<String,Object>>)prm.get("rc");
+		HashMap<String,Object> hm = list.get(0);
+		model.addAttribute("memberList", hm);
+		return "갈곳경로;
+	}
+	*/
 	
 	
 }
