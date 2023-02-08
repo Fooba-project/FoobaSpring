e0cd1cafe686f6d095c812448d9d59bc08a9f174
Isaac Jeong <isacgood@naver.com>
2023-02-08 16:47:11 +0900

    aa

---
+++ FoobaSprings/src/main/java/fooba/FoobaSpringsApplication.java
@@ -0,0 +1,13 @@
+package fooba;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FoobaSpringsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FoobaSpringsApplication.class, args);
+	}
+
+}
---
+++ FoobaSprings/src/test/java/fooba/FoobaSpringsApplicationTests.java
@@ -0,0 +1,13 @@
+package fooba;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FoobaSpringsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
