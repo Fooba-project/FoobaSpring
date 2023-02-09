b25398afd02dfb2947be41a1e9eaf48b0e4c9d43
Isaac <Isaac@DESKTOP-URA3RSE>
2023-02-09 13:59:54 +0900

    aaa

--- FoobaSpring/src/main/webapp/Database/isaac.sql
+++ FoobaSpring/src/main/webapp/Database/isaac.sql
@@ -9,28 +9,28 @@ BEGIN
 END;
 
 create or replace procedure adminGetAllCount(
-p_tableName in number,
+p_table in varchar2,
 p_key in varchar2,
 p_cnt out number
 )
 IS
 BEGIN
-if p_tableName = 'r' then
+if p_table= 'r' then
     select count(*) into p_cnt from restaurant where rname like '%'|| p_key ||'%';
-elsif p_tableName='o' then
+elsif p_table='o' then
     select count(*) into p_cnt from order_view where id like '%'|| p_key ||'%' or rname like '%'|| p_key ||'%';
-elsif p_tableName='m' then
+elsif p_table='m' then
     select count(*) into p_cnt from member where id like '%'|| p_key ||'%';
-elsif p_tableName='q' then
+elsif p_table='q' then
     select count(*) into p_cnt from qna where content like '%'|| p_key ||'%' or subject like '%'|| p_key ||'%';
-elsif p_tableName = 'b' then
+elsif p_table= 'b' then
     select count(rownum) into p_cnt from banner;
 end if;
 END;
 
 
 create or replace procedure adminList(
-p_tableName in number,
+p_table in varchar2,
 p_key in varchar2,
 p_startNum in number,
 p_endNum in number,
@@ -38,19 +38,19 @@ p_rc out sys_refcursor
 )
 IS
 BEGIN
-if p_tableName = 'r' then
+if p_table= 'r' then
     open p_rc for
     select * from (select * from (select rownum as rn, b.* from ((select * from restaurant where rname like '%'|| p_key ||'%' order by rseq desc) b)) where rn>=p_startNum) where rn<=p_endNum;
-elsif p_tableName = 'o' then
+elsif p_table= 'o' then
     open p_rc for
     select * from (select * from (select rownum as rn, b.* from ((select * from order_view where id like '%'|| p_key ||'%' or rname like '%'|| p_key ||'%' order by odseq desc) b)) where rn>=p_startNum) where rn<=p_endNum;
-elsif p_tableName = 'm' then
+elsif p_table= 'm' then
     open p_rc for
     select * from (select * from (select rownum as rn, b.* from ((select * from member where id like '%'|| p_key ||'%' order by indate desc) b)) where rn>=p_startNum) where rn<=p_endNum;
-elsif p_tableName = 'q' then
+elsif p_table= 'q' then
     open p_rc for
     select * from (select * from (select rownum as rn, b.* from ((select * from qna where content like '%'|| p_key ||'%' or subject like '%'|| p_key ||'%' order by qseq desc) b)) where rn>=p_startNum) where rn<=p_endNum;
-elsif p_tableName = 'b' then
+elsif p_table= 'b' then
     open p_rc for
     select * from banner order by useyn desc, order_seq asc;
 end if;
