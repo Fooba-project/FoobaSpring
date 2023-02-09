ea10b4b749cc717d01e189ce79a82159d3d5957b
JAVA01 <JAVA01@DESKTOP-6IFCD5A>
2023-02-09 13:30:57 +0900

    미니 로그인 컨트롤 작성

--- FoobaSpring/src/main/java/fooba/controller/MemberController2.java
+++ FoobaSpring/src/main/java/fooba/controller/MemberController2.java
@@ -14,6 +14,7 @@ import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import fooba.dto.MemberVO;
 import fooba.service.MemberService2;
@@ -30,7 +31,8 @@ public class MemberController2 {
 	public String method(@ModelAttribute("dto") @Valid MemberVO mvo, BindingResult result,
 			HttpServletRequest request, HttpSession session, Model model,
 			@RequestParam("인수1") String 인수1) {
-		if(session.getAttribute("loginUser")==null) return "redirect:/login"; // 로그인체크
+		if(session.getAttribute("loginUser")==null) 
+		return "redirect:/login"; // 로그인체크
 
 		HashMap<String, Object> prm = new HashMap<>();
 		prm.put("인수1", 인수1);
@@ -43,43 +45,38 @@ public class MemberController2 {
 	}
 	*/
 	
-	@RequestMapping("/index")
-	public String index() {
-		return "main";
-	}
 	
-	@RequestMapping(value="/loginForm")
+	@RequestMapping("/loginForm")
 	public String loginForm() {
 		return "member/memberLogin";
 	}
 	
 	 @RequestMapping(value="login", method=RequestMethod.POST)
-	   public String login(@ModelAttribute("dto") @Valid MemberVO membervo, BindingResult result, 
-	         HttpServletRequest request, Model model) {
+	   public String login(@ModelAttribute("dto") @Valid MemberVO membervo, 
+			   BindingResult result, HttpServletRequest request, Model model) {
 	      
 	      String url = "member/memberLogin";
-	      if(result.getFieldError("id")!=null)
-	         model.addAttribute("message", result.getFieldError("id").getDefaultMessage());
-	      else if (result.getFieldError("pwd")!=null)
-	         model.addAttribute("message", result.getFieldError("pwd").getDefaultMessage());
+	      if(result.getFieldError("userid")!=null)
+	         model.addAttribute("message", result.getFieldError("userid").getDefaultMessage());
+	      else if (result.getFieldError("userpw")!=null)
+	         model.addAttribute("message", result.getFieldError("userpw").getDefaultMessage());
 	      else {
 	         HashMap<String, Object> paramMap = new HashMap<String, Object>();
-	         paramMap.put("id", membervo.getId());
+	         paramMap.put("userid", membervo.getId());
 	         paramMap.put("ref_cursor", null);
 	         
 	         ms.getMember(paramMap);
 	         
 	         ArrayList<HashMap<String,Object>> list 
 	         = (ArrayList<HashMap<String,Object>>)paramMap.get("ref_cursor");
-	         //System.out.println(list.get(0).get("ID"));
 	         if(list==null) {	
 	            model.addAttribute("message","아이디가 없습니다.");
 	            return "member/memberLogin";   
 	         }
 	         HashMap<String, Object> mvo = list.get(0);
-	         if(!mvo.get("PWD").equals(membervo.getPwd()))
+	         if(!mvo.get("USERPW").equals(membervo.getPwd()))
 	            model.addAttribute("message","비번이 안맞습니다.");
-	         else if (mvo.get("PWD").equals(membervo.getPwd())) {
+	         else if (mvo.get("USERPW").equals(membervo.getPwd())) {
 	            HttpSession session = request.getSession();
 	            session.setAttribute("loginUser", mvo);
 	            url = "redirect:/";
@@ -88,12 +85,56 @@ public class MemberController2 {
 	      return url;
 	 }
 	
-	 @RequestMapping(value="/logout")
+	 @RequestMapping("/logout")
 	 public String logout(HttpServletRequest request) {
 		 HttpSession session = request.getSession();
 		 session.removeAttribute("loginUser");
 		 return "redirect:/";
 	 }
 	 
+	 @RequestMapping("/miniLoginForm")
+	 public String mini_login_form( HttpServletRequest request) {
+		 if(request.getParameter("check")==null)
+			 request.setAttribute("message", "로그인이 필요합니다.");
+		 return "member/memberMiniLogin";
+	 }
+	 
+	 @RequestMapping("/miniLogin")
+	 public String miniLogin(@ModelAttribute("dto") @Valid MemberVO membervo, BindingResult result, 
+	         HttpServletRequest request, Model model,
+	         @RequestParam("pseq") String pseq) {
+		 
+		 String url="member/memberMiniLogin";
+		 
+		 model.addAttribute("check", "1");
+		 
+		 if(result.getFieldError("userid")!=null)
+	         model.addAttribute("message", result.getFieldError("userid").getDefaultMessage());
+	      else if (result.getFieldError("userpw")!=null)
+	         model.addAttribute("message", result.getFieldError("userpw").getDefaultMessage());
+	      else {
+	         HashMap<String, Object> paramMap = new HashMap<String, Object>();
+	         paramMap.put("userid", membervo.getId());
+	         paramMap.put("ref_cursor", null);
+	         
+	         ms.getMember(paramMap);
+	         
+	         ArrayList<HashMap<String,Object>> list 
+	         = (ArrayList<HashMap<String,Object>>)paramMap.get("ref_cursor");
+	         if(list==null) {	
+	            model.addAttribute("message","아이디가 없습니다.");
+	            return "member/memberLogin";   
+	         }
+	         HashMap<String, Object> mvo = list.get(0);
+	         if(!mvo.get("USERPW").equals(membervo.getPwd()))
+	            model.addAttribute("message","비번이 안맞습니다.");
+	         else if (mvo.get("USERPW").equals(membervo.getPwd())) {
+	            HttpSession session = request.getSession();
+	            session.setAttribute("loginUser", mvo);
+	            url = "redirect:/";
+	         }	   	 
+	      }
+		 return url;
+	 }
 	 
 }
