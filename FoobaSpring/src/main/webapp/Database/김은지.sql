d981f05bb632bbf38b60ddcfe1a620c3a0c04479
JAVA01 <JAVA01@DESKTOP-6IFCD5A>
2023-02-10 09:26:01 +0900

    자잘구리 수정

--- FoobaSpring/src/main/webapp/Database/김은지.sql
+++ FoobaSpring/src/main/webapp/Database/김은지.sql
@@ -5,12 +5,16 @@ BEGIN
 END;
 
 
+CREATE OR REPLACE  PROCEDURE FimagebyRseq(p_rseq IN NUMBER, p_fimage OUT varchar2 )
+IS
+BEGIN
+         select distinct fimage INTO p_fimage from foodmenu where rseq=p_rseq;
+END;
+
 CREATE OR REPLACE  PROCEDURE SearchResList(p_searchtext IN varchar2, p_key OUT SYS_REFCURSOR )
 IS
-    v_key  SYS_REFCURSOR;
 BEGIN
-    OPEN  v_key  FOR 
+    OPEN  p_key  FOR 
          select rname, rseq, rimage, kind, hash from search  where ryn in(1,3) 
          and (fname like '%'||p_searchtext||'%' or hash like '%'||p_searchtext||'%'  or  rname like '%'||p_searchtext||'%') group by rname,rseq,rimage,kind,hash;       
 END;
-
