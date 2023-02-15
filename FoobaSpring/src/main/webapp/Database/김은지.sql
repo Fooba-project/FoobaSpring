982e81a31d7a0aa6b4dcefc8d1d5538391897e0e
JAVA01 <JAVA01@DESKTOP-6IFCD5A>
2023-02-15 17:16:01 +0900

    오더 인서트 완성

--- FoobaSpring/src/main/webapp/Database/김은지.sql
+++ FoobaSpring/src/main/webapp/Database/김은지.sql
@@ -133,34 +133,61 @@ END;
 --    commit;
 --END;
 
- CREATE OR REPLACE PROCEDURE insertOrdersFooba( p_id IN orders.id%type, p_rideryn IN orders.rideryn%type,
+ CREATE OR REPLACE PROCEDURE insertOrders(p_rseq IN orders.rseq%type, p_id IN orders.id%type, p_rideryn IN orders.rideryn%type,
     p_plasticyn IN orders.plasticyn%type, p_payment IN orders.payment%type, p_address1  IN orders.address1%type, p_address2  IN orders.address2%type,
-    p_address3  IN orders.address3%type, p_phone  IN orders.phone%type,p_totalprice  IN orders.totalprice%type,    p_oseq OUT orders.oseq%type )
+    p_phone  IN orders.phone%type,p_totalprice  IN orders.totalprice%type,    p_oseq OUT orders.oseq%type )
 IS
-    v_oseq orders.oseq%type;
     temp_cur SYS_REFCURSOR;
+    v_oseq orders.oseq%type;
     v_cseq cart.cseq%type;
-    v_fseq cart.fseq%type;
     v_quantity cart.quantity%type;
+    v_fseq cart.fseq%type;
     v_sideyn1 cart.sideyn1%type;
     v_sideyn2 cart.sideyn2%type;
     v_sideyn3 cart.sideyn3%type;
+    v_sideyn1_1 number;
+    v_sideyn2_1 number;
+    v_sideyn3_1 number;
 BEGIN  
-    INSERT INTO orders(oseq, id, rideryn, plasticyn, payment, address1, address2, address3, phone, totalprice)
-    VALUES(orders_seq.nextVal, p_id, p_rideryn, p_plasticyn, p_payment, p_address1, p_address2, p_address3, p_phone, p_totalprice);
-   select max(oseq) into v_oseq from orders where id=p_id;
-     OPEN temp_cur FOR SELECT cseq, fseq, quantity FROM cart WHERE id = p_id ;    
+    INSERT INTO orders(oseq, rseq, id, rideryn, plasticyn, payment, address1, address2, phone, totalprice)
+    VALUES(orders_seq.nextVal, p_rseq, p_id, p_rideryn, p_plasticyn, p_payment, p_address1, p_address2, p_phone, p_totalprice);
+    select max(oseq) into v_oseq from orders where id=p_id;
+     OPEN temp_cur FOR SELECT cseq, quantity, fseq, sideyn1, sideyn2, sideyn3 FROM cart WHERE id = p_id ;    
     LOOP
-        FETCH temp_cur INTO v_cseq, v_fseq, v_quantity;
+        FETCH temp_cur INTO v_cseq, v_fseq, v_quantity, v_sideyn1, v_sideyn2, v_sideyn3 ;
         EXIT WHEN temp_cur%NOTFOUND;        --20มู
+        if(v_sideyn1 is null) then v_sideyn1_1 := 0;
+        else v_sideyn1_1 := 1;
+        end if;
+        if(v_sideyn1 is null) then v_sideyn1_1 := 0;
+        else v_sideyn1_1 := 1;
+        end if;
+        if(v_sideyn1 is null) then v_sideyn1_1 := 0;
+        else v_sideyn1_1 := 1;
+        end if;
         INSERT INTO order_detail(odseq, oseq, quantity, fseq, sideyn1, sideyn2, sideyn3 )
-        VALUES(order_detail_seq.nextVal, v_oseq, v_quantity, v_fseq, v_sideyn1, v_sideyn2, v_sideyn3);
-        DELETE FROM cart WHERE id=p_id;
+        VALUES(order_detail_seq.nextVal, v_oseq, v_quantity, v_fseq, v_sideyn1_1, v_sideyn2_1, v_sideyn3_1);
+        DELETE FROM cart WHERE id=p_id;       
     END LOOP;
     commit;
     p_oseq := v_oseq;
 END;
 
+alter table orders add oname varchar2(100);
 
 
-select*from orders where id='bsc1234';
+create or replace view order_view
+as
+select a.oseq, a.result, a.oname, a.indate, a.id, a.rideryn, a.plasticyn, a.payment, a.address1 as oadd1, a.address2 as oadd2, a.phone as ophone, a.totalprice,
+      b.odseq, b.quantity, b.fseq, b.sideyn1, b.sideyn2, b.sideyn3,
+      c.nick, c.address1 as madd1, c.address2 as madd2, c.phone as mphone,
+      d.fname, d.fprice, d.fside1, d.fside2, d.fside3, d.fsideprice1, d.fsideprice2, d.fsideprice3, d.fimage, d.fcontent,
+      e.rname, e.rseq, e.rimage, e.rtip, e.ryn
+from orders a, order_detail b, member c, foodmenu d, restaurant e
+where a.oseq=b.oseq and a.id = c.id and b.fseq=d.fseq and d.rseq=e.rseq;
+
+select*from order_detail ;
+select*from cart;
+
+INSERT INTO order_detail(odseq, oseq, quantity, fseq, sideyn1, sideyn2, sideyn3 )
+VALUES(order_detail_seq.nextVal, 1, v_quantity, v_fseq, v_sideyn1, v_sideyn2, v_sideyn3);
