1f21756ef10b8d3d4291059cecfb0c8c4ca87394
JAVA01 <JAVA01@DESKTOP-6IFCD5A>
2023-02-16 09:51:22 +0900

    메인 오류 수정

--- FoobaSpring/src/main/java/fooba/controller/MemberController2.java
+++ FoobaSpring/src/main/java/fooba/controller/MemberController2.java
@@ -36,30 +36,6 @@ public class MemberController2 {
 	@Autowired
 	ResService rs;
 	
-	@RequestMapping("/index")
-	public String index() {
-		return "main";
-	}
-	
-	/*
-	// 기본 프레임
-	@RequestMapping("/")
-	public String method(@ModelAttribute("dto") @Valid MemberVO mvo, BindingResult result,
-			HttpServletRequest request, HttpSession session, Model model,
-			@RequestParam("인수1") String 인수1) {
-		if(session.getAttribute("loginUser")==null) 
-		return "redirect:/login"; // 로그인체크
-
-		HashMap<String, Object> prm = new HashMap<>();
-		prm.put("인수1", 인수1);
-		prm.put("rc", null);
-		ms.servicemethod(hm);
-		ArrayList<HashMap<String,Object>> list = (ArrayList<HashMap<String,Object>>)prm.get("rc");
-		HashMap<String,Object> hm = list.get(0);
-		model.addAttribute("memberList", hm);
-		return "갈곳경로;
-	}
-	*/
 	
 	@RequestMapping("/loginForm")
 	public String loginForm() {
